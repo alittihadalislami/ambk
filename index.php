82132011ffdf81d005998d6a4a10cad2e4d933b8
fu4d1-bismillah <alittihadalislami@gmail.com>
2023-03-14 08:00:27 +0700

    Update index.php
    refresh setiap 30 detik

--- index.php
+++ index.php
@@ -3,10 +3,11 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="refresh" content="30">
     <title>AMBK - MA Al Ittihad Al Islami</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
   </head>
-  <body>
+  <body >
     <?php 
         $jadwal = [
             ["Senin, 13 Maret 2023. 07.30-09.00", "https://tinyurl.com/ambkski", "SKI"],
