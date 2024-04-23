692d6860242b5833609074195655810e462c4113
Nurul Fuadi <alittihadalislami@gmail.com>
2024-04-23 10:04:57 +0700

    Create index.php

---
+++ prakarya/index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PRAKTIK PRAKARYA</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <style>
+        body {
+            background: rgb(99,224,196);
+            background: linear-gradient(90deg, rgba(125,226,203,1) 54%, rgba(255,255,255,1) 100%);
+        }
+    </style>
+</head>
+<body>
+
+    <div class="container">
+        <div class="row">
+            <h1 class="mt-4"> Ujian Praktik TIK-PRAKARYA</h1>
+        </div>
+        
+        <div class="row mt-4">
+            <div class="col-8">
+                <table class="table table-striped">
+                    <tbody>
+                        <tr>
+                            <td class="col-3">Madrasah</td>
+                            <td class="fw-bold fs-5">MA AL ITTIHAD AL ISLAMI</td>
+                        </tr>
+                        <tr>
+                            <td>Tahun</td>
+                            <td>2024</td>
+                        </tr>
+                        <tr>
+                            <td>Mata Pelajaran</td>
+                            <td>TIK - Prakarya</td>
+                        </tr>
+                        <tr>
+                            <td>Hari, Tanggal dan Jam</td>
+                            <td>Senin, 20-12-2024</td>
+                        </tr>
+                        <tr>
+                            <td>Token</td>
+                            <td>20241220.45456</td>
+                        </tr>
+                        <tr>
+                            <td>Nama</td>
+                            <td>Ahmad Mustofa</td>
+                        </tr>
+                        <tr>
+                            <td>Soal Praktek</td>
+                            <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, quos quisquam. Vitae ipsum optio provident, nostrum, totam similique aliquam, laborum rem magni error labore doloribus sequi neque quidem unde sit.</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script>
+
+    </script>
+</body>
+</html>
